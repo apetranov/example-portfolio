be53b2bb5d8c5d90e3435ca8c99430ad2fa11dab
Alexander Petranov <shasho68.5@gmail.com>
2024-09-19 21:31:22 +0300

    Fix light dark toggle icon on mobile

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelector(".links");
   const close = document.querySelector(".close");
   const mobile = document.querySelector(".mobile");
+  const lightBtn2 = document.querySelector(".light2");
+  const darkBtn2 = document.querySelector(".dark2");
 
   menu.addEventListener("click", () => {
     console.log("Menu clicked");
@@ -36,10 +38,12 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   modeSwitch2.addEventListener("click", () => {
-    if (!lightBtn.classList.contains("hidden")) {
-      lightBtn.classList.add("hidden");
-      darkBtn.classList.remove("hidden");
+    if (!lightBtn2.classList.contains("hidden")) {
+      lightBtn2.classList.add("hidden");
+      darkBtn2.classList.remove("hidden");
       modeSwitch2.style.backgroundColor = "gray";
+      modeSwitch2.style.padding = "5px";
+      modeSwitch2.style.borderRadius = "100%";
       body.classList.add("dark-mode");
       link.style.color = "white";
       close.style.backgroundColor = "gray";
@@ -47,8 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
       close.style.borderRadius = "100%";
       modeSwitch.style.backgroundColor = "gray";
     } else {
-      darkBtn.classList.add("hidden");
-      lightBtn.classList.remove("hidden");
+      darkBtn2.classList.add("hidden");
+      lightBtn2.classList.remove("hidden");
       body.classList.remove("dark-mode");
       link.style.color = "black";
       modeSwitch2.style.backgroundColor = "white";
